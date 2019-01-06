fb945cc91036cef3e7cb64045556bbf12c731fa4
Rafnsvart <rafnsvart.d@yandex.ru>
2019-01-07 01:19:13 +0500

    Вынесение в отдельную функцию

--- Web-API/Web-API/Form1.cs
+++ Web-API/Web-API/Form1.cs
@@ -70,8 +70,88 @@ namespace Web_API
                 salary = (double)salaryFrom / rate;
             return salary;
         }
+
+
+
+
+        private List<string> RABOTAY_BLE(bool big_salary, bool details)
+        {
+            IRestRequest request;
+
+
+
+
+            List<string> listProf = new List<string>();
+
+
+            if (big_salary)
+            {
+                 request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage, bigSalary), Method.GET);
+            }
+            else
+            {
+                 request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage), Method.GET);
+            }
+            IRestResponse response = _client.Execute(request);   
+            int pagesCount = (int)JObject.Parse(response.Content)["pages"];
+            JArray vacancies = JObject.Parse(response.Content)["items"] as JArray;
+            for (int i = VacanciesFirstPage; i < pagesCount; i++)
+            {
+                foreach (JToken vacancy in vacancies)
+                {
+                    JToken salaryCurr = vacancy["salary"]["currency"];
+
+                    double salary = 0;
+                    if ((string)salaryCurr != "RUR")
+                        salary = translate(vacancy);
+                    else
+                        salary = sumSalary(vacancy);
+                    if (!details)
+                    {
+
+                        if (big_salary && salary >= bigSalary)
+                            listProf.Add((string)vacancy["name"]);
+                        else if (!big_salary && salary <lowSalary && salary >0)
+                            listProf.Add((string)vacancy["name"]);
+                    }
+                    else
+                    {
+                        if ((big_salary && salary >= bigSalary) || (!big_salary && salary < lowSalary && salary >0))
+                        {
+                            IRestRequest request_details = new RestRequest(string.Format("{0}/{1}", HhApiVacanciesResource, (string)vacancy["id"]), Method.GET);
+                            IRestResponse response_details = _client.Execute(request_details);
+
+                            JToken vacancyDetails = JObject.Parse(response_details.Content);
+                            JArray keySkills = vacancyDetails["key_skills"] as JArray;
+                            if (keySkills.HasValues)
+                            {
+                                foreach (JToken keySkill in keySkills)
+                                {
+                                    listProf.Add((string)keySkill["name"]);
+                                }
+                            }
+                        }
+                    }
+                }
+                int FirstPage = VacanciesFirstPage + i;
+                if (big_salary)
+                {
+                    request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, FirstPage, VacanciesPerPage, bigSalary), Method.GET);
+                }
+                else
+                {
+                    request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&only_with_salary=true", HhApiVacanciesResource, FirstPage, VacanciesPerPage), Method.GET);
+                }
+                response = _client.Execute(request);
+                vacancies = JObject.Parse(response.Content)["items"] as JArray;
+            }
+            return listProf;
+        }
+
         private async void button1_Click(object sender, EventArgs e)
         {
+            listBox1.DataSource = RABOTAY_BLE(true,false);
+            /*
             //listProf _listi_ = new listProf();
             List<string> listProf = new List<string>();
 
@@ -102,35 +182,13 @@ namespace Web_API
             }
 
             listBox1.DataSource = listProf;
-
-
-            /*
-            using (HttpClient client = new HttpClient())
-            {
-                // Call asynchronous network methods in a try/catch block to handle exceptions
-                try
-                {
-                    HttpResponseMessage response = await client.GetAsync("https://api.hh.ru/vacancies/?page=0&per_page=100&only_with_salary=true&salary=120000");
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    // Above three lines can be replaced with new helper method below
-                    // string responseBody = await client.GetStringAsync(uri);
-
-                    Console.WriteLine(responseBody);
-                }
-                catch (HttpRequestException ex)
-                {
-                    Console.WriteLine("\nException Caught!");
-                    Console.WriteLine("Message :{0} ", ex.Message);
-                }
-                }
-     * */
-
-
+            */
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
+            listBox2.DataSource = RABOTAY_BLE(true,true);
+            /*
             List<string> listProf = new List<string>(); 
 
             IRestRequest request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage, bigSalary), Method.GET);
@@ -165,10 +223,6 @@ namespace Web_API
                         }
                         
                     }
-
-                   
-
-
                 }
                 int FirstPage = VacanciesFirstPage + i;
                 request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, FirstPage, VacanciesPerPage, bigSalary), Method.GET);
@@ -177,10 +231,14 @@ namespace Web_API
             }
 
             listBox2.DataSource = listProf;
+            */
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+
+            listBox3.DataSource = RABOTAY_BLE(false,false);
+            /*
             List<string> listProf = new List<string>();
 
             IRestRequest request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage), Method.GET);
@@ -210,10 +268,14 @@ namespace Web_API
             }
 
             listBox3.DataSource = listProf;
+            */
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+
+            listBox4.DataSource = RABOTAY_BLE(false, true);
+            /*
             List<string> listProf = new List<string>();
 
             IRestRequest request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage), Method.GET);
@@ -260,6 +322,7 @@ namespace Web_API
             }
 
             listBox4.DataSource = listProf;
+            */
         }
     }
 
