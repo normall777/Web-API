044bc22df4517221bbb0232e4df1a57557a5e91d
Rafnsvart <rafnsvart.d@yandex.ru>
2019-01-06 19:37:34 +0500

    Кнопка 1

--- Web-API/Web-API/Form1.cs
+++ Web-API/Web-API/Form1.cs
@@ -8,18 +8,66 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json.Linq;
 
 namespace Web_API
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Переменные для запроса
+        /// </summary>
+        const string HhApiHost = "https://api.hh.ru"; //Куда
+        const string HhApiVacanciesResource = "/vacancies"; //Что
+        const int VacanciesPerPage = 100; //Число вакансий на странице (500 не работает)
+        const int VacanciesFirstPage = 0; //Начать с первой страницы
+        const int bigSalary = 120000;
+        const int lowSalary = 15000;
+        readonly IRestClient _client = new RestClient(HhApiHost);
+
+        public class listProf
+        {
+            public listProf()
+            {           
+                __list = new List<String>();         
+            }
+            public List<String> __list { get; private set; }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
+
+
+
         private async void button1_Click(object sender, EventArgs e)
         {
+            listProf _listi_ = new listProf();
+
+            IRestRequest request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, VacanciesFirstPage, VacanciesPerPage, bigSalary), Method.GET);
+            IRestResponse response = _client.Execute(request);    //RequestVacancies(VacanciesFirstPage);
+            int pagesCount = (int)JObject.Parse(response.Content)["pages"];
+            JArray vacancies = JObject.Parse(response.Content)["items"] as JArray;
+            for (int i = VacanciesFirstPage; i < pagesCount; i++)
+            {
+                foreach (JToken vacancy in vacancies)
+                {
+                    _listi_.__list.Add((string)vacancy["name"]);
+
+                }
+                int FirstPage = VacanciesFirstPage + i;
+                request = new RestRequest(string.Format("{0}?page={1}&per_page={2}&salary={3}&only_with_salary=true", HhApiVacanciesResource, FirstPage, VacanciesPerPage, bigSalary), Method.GET);
+                response = _client.Execute(request);
+                vacancies = JObject.Parse(response.Content)["items"] as JArray;
+            }
+
+            listBox1.DataSource = _listi_.__list;
+
+
+            /*
             using (HttpClient client = new HttpClient())
             {
                 // Call asynchronous network methods in a try/catch block to handle exceptions
@@ -38,7 +86,14 @@ namespace Web_API
                     Console.WriteLine("\nException Caught!");
                     Console.WriteLine("Message :{0} ", ex.Message);
                 }
-            }
+                }
+     * */
+
+
         }
+
+
     }
+
 }
+
